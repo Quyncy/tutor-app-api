cbfa15015c214602bded418022257d9fd76293df
Quynh <quynhmai@gmx.de>
2022-07-20 19:34:53 +0200

    Add listing users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,5 @@
 """
-Test for the django admin modification
+Tests for the Django admin modifications.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -7,16 +7,15 @@ from django.urls import reverse
 from django.test import Client
 
 
-class AdminSiteTest(TestCase):
-    """Test for Django admin."""
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
 
     def setUp(self):
-        """Create user and client"""
+        """Create user and client."""
         self.client = Client()
-        # admin erstellen
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@example.com',
-            password = 'testpass123',
+            email='admin@example.com',
+            password='testpass123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
@@ -25,12 +24,10 @@ class AdminSiteTest(TestCase):
             name='Test User'
         )
 
-    def test_user_lists(self):
-
-        url = reverse('admin:core_user_changeList')
+    def test_users_lists(self):
+        """Test that users are listed on page."""
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
-
-
